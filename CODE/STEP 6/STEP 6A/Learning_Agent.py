672fb8c2bd48ffd653f3628e8ad2d4063cb26951
Carlo000ml <112633216+Carlo000ml@users.noreply.github.com>
2023-09-22 20:14:17 +0200

    Add files via upload

---
+++ CODE/STEP 6/STEP 6A/Learning_Agent.py
@@ -0,0 +1,20 @@
+import numpy as np
+class Learning_Agent:
+    
+    def __init__(self , arms):
+        self.arms=arms
+        self.n_arms=len(arms)
+        self.arms_indexing={arms[i] : i for i in range(self.n_arms) }
+        self.t=0  # round indicator
+        self.reward_per_arm={arm : [] for arm in arms}
+        self.collected_reward=np.array([])
+        
+    def pull_arm(self):
+        pass
+    
+    def collect_reward(self, pulled_arm, reward):
+        self.reward_per_arm[pulled_arm].append(reward)
+        self.collected_reward = np.append(self.collected_reward, reward)
+        
+    def update(self):
+        pass
