ecca669c2fd975ececa916166c1b16cdf3ebf09d
Carlo000ml <112633216+Carlo000ml@users.noreply.github.com>
2023-09-22 10:21:34 +0200

    Add files via upload

---
+++ CODE/STEP 3/Class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Class:
+    def __init__(self): 
+        
+        class_1=(1,1) # <=30 , urban
+        details_1={'features': (1,1) , 'prob' :{10:0.4,15:0.6,20:0.3,25:0.2,30:0.1} , 'num_of_click_params' :(3,85,10000)  , 'cumulative_cost_params': 700
+                , 'class_description': "less than 30  &  urban"}
+        
+        self.classes={class_1 : details_1 }
+            
+        
+
---
+++ CODE/STEP 3/TS.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from Learning_Agent import Learning_Agent
+import numpy as np
+
+class TS(Learning_Agent):
+    
+    def __init__(self, arms):
+        super().__init__(arms)
+        
+        self.beta_params=np.ones([len(arms) , 2])
+        
+    def pull_arm(self):
+        
+        means=np.random.beta(self.beta_params[:,0] , self.beta_params[:,1])
+        
+        return [self.arms[np.argmax(means* self.arms)] ,means[np.argmax(means* self.arms)] ]
+    
+    def update(self, pulled_arm , reward):
+        self.t+=1
+        pulled_arm_index=self.arms_indexing[pulled_arm]
+        
+        successes=reward[0]
+        fails=reward[1]-successes
+        
+        self.beta_params[pulled_arm_index][0]=self.beta_params[pulled_arm_index][0]+successes
+        self.beta_params[pulled_arm_index][1]=self.beta_params[pulled_arm_index][1]+fails
+        self.collect_reward(pulled_arm , reward[2])
+       
+
+
+# In[ ]:
+
+
+
+
